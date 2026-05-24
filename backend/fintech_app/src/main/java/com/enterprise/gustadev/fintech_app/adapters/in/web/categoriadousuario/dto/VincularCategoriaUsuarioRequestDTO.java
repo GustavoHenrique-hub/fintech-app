30d9b696cebf08b06874a645da62473a7a3f2c5e
Gustavo Henrique <gustavosilvah37@gmail.com>
2026-05-24 10:40:18 -0300

    fix: CategoriaDoUsuarioController.java, CategoriaDoUsuarioResponseDTO.java, VincularCategoriaUsuarioRequestDTO.java, CategoriaDoUsuarioEntity.java, CategoriaDoUsuarioJpaRepository.java, CategoriaDoUsuarioRepositoryAdapter.java, ...

--- backend/fintech_app/src/main/java/com/enterprise/gustadev/fintech_app/adapters/in/web/categoriadousuario/dto/VincularCategoriaUsuarioRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.enterprise.gustadev.fintech_app.adapters.in.web.categoriadousuario.dto;
-
-import jakarta.validation.constraints.NotNull;
-
-
-public record VincularCategoriaUsuarioRequestDTO(
-        @NotNull Long usuarioId,
-        @NotNull Long categoriaId
-) {}
--- backend/fintech_app/src/main/java/com/enterprise/gustadev/fintech_app/domain/categoriadousuario/exception/CategoriaDoUsuarioInvalidaException.java
+++
@@ -1,7 +0,0 @@
-package com.enterprise.gustadev.fintech_app.domain.categoriadousuario.exception;
-
-public class CategoriaDoUsuarioInvalidaException extends RuntimeException {
-    public CategoriaDoUsuarioInvalidaException(String message) {
-        super(message);
-    }
-}
