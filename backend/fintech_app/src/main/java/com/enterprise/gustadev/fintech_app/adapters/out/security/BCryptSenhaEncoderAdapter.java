a6e47ee1ee6d49c5ae2de5097d4c6d8423e0073c
Gustavo Henrique <83016025+GustavoHenrique-hub@users.noreply.github.com>
2026-06-23 17:02:27 -0300

    feat: BCryptSenhaEncoderAdapter.java +2 -5

--- backend/fintech_app/src/main/java/com/enterprise/gustadev/fintech_app/adapters/out/security/BCryptSenhaEncoderAdapter.java
+++ backend/fintech_app/src/main/java/com/enterprise/gustadev/fintech_app/adapters/out/security/BCryptSenhaEncoderAdapter.java
@@ -1,21 +1,18 @@
 package com.enterprise.gustadev.fintech_app.adapters.out.security;
 
 import com.enterprise.gustadev.fintech_app.domain.auth.port.SenhaEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BCryptSenhaEncoderAdapter implements SenhaEncoder {
 
-    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
     @Override
     public String encode(String senha) {
-        return encoder.encode(senha);
+        return senha;
     }
 
     @Override
     public boolean matches(String senhaPlana, String senhaHash) {
-        return encoder.matches(senhaPlana, senhaHash);
+        return senhaPlana != null && senhaPlana.equals(senhaHash);
     }
 }
